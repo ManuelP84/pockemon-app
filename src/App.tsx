ed1aad5ed65a8663c8d55d816682472d9a576d45
Manuel Pineda <pineda.manuel@gmail.com>
2022-07-14 11:49:08 -0500

    Remove the components from the App.tsx and leave the Router components only

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-//import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import PockemonList from './components/PockemonList'
-import Favorites from './components/Favorites'
-import Login from './components/Login'
-import PockemonFilter from './components/PockemonFilter'
-import PockemonCard from './components/PockemonCard'
-import PockemonDetail from './components/PockemonDetail'
-import PockemonNavbar from './components/Navbar'
+import Router from './components/routes/Router'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <div>
-      <PockemonNavbar/>
-      <Login/>
-      <PockemonFilter/>      
-      <PockemonList/>
-      <PockemonDetail/>
-      <Favorites/>
-      <PockemonCard/>
+      <Router/>
     </div>
   )
 }
