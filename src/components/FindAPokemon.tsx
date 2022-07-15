284042cae01f4d9a7329d33ed9b7abef73ef78f7
Manuel Pineda <pineda.manuel@gmail.com>
2022-07-15 15:44:19 -0500

    Pokemon filter functionallity adition!

--- src/components/FindAPokemon.tsx
+++ src/components/FindAPokemon.tsx
@@ -16,7 +16,7 @@ const FindAPokemon: React.FunctionComponent<IFindAPokemonProps> = (props) => {
   const [word, setWord] = React.useState("");
   const dispatch = appDispatch();
 
-    const filterList = pokemons.filter((poke) => poke.name.includes(word));
+  const filterList = pokemons.filter((poke) => poke.name.includes(word));
 
   return (
     <>
@@ -34,59 +34,60 @@ const FindAPokemon: React.FunctionComponent<IFindAPokemonProps> = (props) => {
         </Form>
         <br />
         <Row xm={1} md={5} className="g-4">
-          {word.length > 0 && filterList.map((pokemon) => (
-            <>
-              <Col>
-                <>
-                  <Card style={{ width: "13rem" }}>
-                    <Card.Body>
-                      <Card.Title>{pokemon.id}</Card.Title>
-                      <Form.Group
-                        className="mb-3"
-                        controlId="formBasicCheckbox"
-                      >
-                        <Form.Check
-                          type="checkbox"
-                          label="Like"
-                          checked={pokemon.isFavorite}
-                          onClick={() =>
-                            dispatch(addFavoriteReducer(pokemon.id))
-                          }
+          {word.length > 0 &&
+            filterList.map((pokemon) => (
+              <>
+                <Col>
+                  <>
+                    <Card style={{ width: "13rem" }}>
+                      <Card.Body>
+                        <Card.Title>{pokemon.id}</Card.Title>
+                        <Form.Group
+                          className="mb-3"
+                          controlId="formBasicCheckbox"
+                        >
+                          <Form.Check
+                            type="checkbox"
+                            label="Like"
+                            checked={pokemon.isFavorite}
+                            onClick={() =>
+                              dispatch(addFavoriteReducer(pokemon.id))
+                            }
+                          />
+                        </Form.Group>
+                        <Card.Img
+                          variant="top"
+                          src={pokemon.sprite}
+                          alt={`Picture from the pokémon ${
+                            pokemon.name.charAt(0).toUpperCase +
+                            pokemon.name.slice(1)
+                          }`}
                         />
-                      </Form.Group>
-                      <Card.Img
-                        variant="top"
-                        src={pokemon.sprite}
-                        alt={`Picture from the pokémon ${
-                          pokemon.name.charAt(0).toUpperCase +
-                          pokemon.name.slice(1)
-                        }`}
-                      />
-                    </Card.Body>
+                      </Card.Body>
 
-                    <Card.Body>
-                      <Card.Title>
-                        <i>
-                          {pokemon.name.charAt(0).toUpperCase() +
-                            pokemon.name.slice(1)}
-                        </i>
-                      </Card.Title>
-                    </Card.Body>
-                    <Card.Body>
-                      <Link
-                        className={
-                          "link-dark btn btn-success btn-lg active p-2"
-                        }
-                        to={`/pockemons`}
-                      >
-                        Pokemons
-                      </Link>
-                    </Card.Body>
-                  </Card>
-                </>
-              </Col>
-            </>
-          ))}
+                      <Card.Body>
+                        <Card.Title>
+                          <i>
+                            {pokemon.name.charAt(0).toUpperCase() +
+                              pokemon.name.slice(1)}
+                          </i>
+                        </Card.Title>
+                      </Card.Body>
+                      <Card.Body>
+                        <Link
+                          className={
+                            "link-dark btn btn-success btn-lg active p-2"
+                          }
+                          to={`/pockemons`}
+                        >
+                          Pokemons
+                        </Link>
+                      </Card.Body>
+                    </Card>
+                  </>
+                </Col>
+              </>
+            ))}
         </Row>
       </Container>
     </>
